b45fb84bf891a66d00173c72535d6357e83a646c
nagayamajun <85071324+nagayamajun@users.noreply.github.com>
2023-09-16 22:18:04 +0900

    ロゴの変更

--- src/components/atoms/Heading/admin/index.tsx
+++ src/components/atoms/Heading/admin/index.tsx
@@ -1,10 +1,16 @@
 import { Routing } from "@/hooks/routing";
+import Image from "next/image";
 import Link from "next/link";
 
 export const AdminHeading = (): JSX.Element => (
   <h1 className="font-bold text-xl">
     <Link href={Routing.adminRentalHouses.buildRoute().path}>
-      Fill Home
+      <Image
+        src='/FiillHome.png'
+        height={160}
+        width={160}
+        alt="ロゴ"
+      />
     </Link>
   </h1>
 )
--- src/components/atoms/Heading/user/index.tsx
+++ src/components/atoms/Heading/user/index.tsx
@@ -1,10 +1,16 @@
 import { Routing } from "@/hooks/routing";
+import Image from "next/image";
 import Link from "next/link";
 
 export const Heading = (): JSX.Element => (
   <h1 className="font-bold text-xl">
     <Link href={Routing.rentalHouses.buildRoute().path}>
-      Fill Home
+      <Image
+        src='/FiillHome.png'
+        height={160}
+        width={160}
+        alt="ロゴ"
+      />
     </Link>
   </h1>
 )

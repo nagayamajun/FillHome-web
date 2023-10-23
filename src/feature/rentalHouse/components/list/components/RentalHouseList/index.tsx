9784e394ebd8ca084d55af38e2e5c32b0f1595ad
nagayamajun <85071324+nagayamajun@users.noreply.github.com>
2023-10-23 17:19:24 +0900

    メインページのレスポンシブの関係を修正

--- src/feature/rentalHouse/components/list/components/RentalHouseList/index.tsx
+++ src/feature/rentalHouse/components/list/components/RentalHouseList/index.tsx
@@ -21,21 +21,19 @@ export const RentalHouseList = ({ rentalHouses }: Props): JSX.Element => {
   });
 
   return (
-    <div className="flex flex-col md:w-2/3 justify-center items-center">
-      <div className="grid gap-y-8 md:grid-cols-2 mt-8 justify-items-center items-center">
-        {filteredHouses?.map((house) => {
-          return (
-            <RentalHouseCard
-              id={house.id}
-              key={house.id}
-              houseName={house.name}
-              img={house.rental_house_photos[0]}
-              address={house.address}
-              rooms={house.mansion?.mansion_rooms}
-            />
-          );
-        })}
-      </div>
+    <div className="grid gap-y-8 sm:gap-x-4 grid-cols-1 sm:grid-cols-2 mt-8 w-[90%] sm:w-[816px] items-center justify-center ">
+      {filteredHouses?.map((house) => {
+        return (
+          <RentalHouseCard
+            id={house.id}
+            key={house.id}
+            houseName={house.name}
+            img={house.rental_house_photos[0]}
+            address={house.address}
+            rooms={house.mansion?.mansion_rooms}
+          />
+        );
+      })}
     </div>
   );
 };
